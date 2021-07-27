c862d30e4a3eb9889ae40119fd8f610c4a1632ab
Mark Francisco <mfrancisco581@gmail.com>
2021-07-27 16:48:25 -0500

    work on emitting event data back to Calendar from AddEvent component

--- src/views/AddEvent.vue
+++ src/views/AddEvent.vue
@@ -3,7 +3,7 @@
     <h1>Add event to: {{ currentlySelectedDate }}</h1>
 
     <b-form-input v-model="title" placeholder="Enter Event Title"></b-form-input>
-    <b-form-input v-model="currentlySelectedDate" placeholder="Enter date"></b-form-input>
+    <b-form-datepicker v-model="currentlySelectedDate" placeholder="Enter date"></b-form-datepicker>
 
     <button>
       <router-link class="m-4 calendar-event-button" to="/">Go Back</router-link>
@@ -20,18 +20,19 @@
 }
 </style>
 <script>
-// import dayjs from "dayjs";
 export default {
   data() {
     return {
-      updatedEventsList: this.events,
+      updatedEvent: { title: null, date: null },
       title: "",
       currentlySelectedDate: this.$route.params.date,
     };
   },
   methods: {
     saveChanges() {
-      // this.events.push(this.text);
+      //use destructuring once this is confirmed working
+      this.updatedEvent["title"] = this.title;
+      this.updatedEvent["date"] = this.currentlySelectedDate;
       this.$router.push({ path: "/" });
     },
   },
